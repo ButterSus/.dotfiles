11d028d1a38fe46ce93d697d4aaa48fb831a7a9e
Krivoshapkin Eduard <eduardkrivosapkin5@gmail.com>
2024-12-05 21:35:15 +0300

    starship added

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,5 @@ set -Ux ELECTRON_OZONE_PLATFORM_HINT wayland
 
 if status is-interactive
     alias dotf "/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME"
+    starship init fish | source
 end
