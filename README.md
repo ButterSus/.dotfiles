321187006ec9fc55597c6da9b671a074353539ba
Krivoshapkin Eduard <buttersus@mail.ru>
2025-04-28 22:47:03 +0300

    feat: add basic developer setup

---
+++ README.md
@@ -0,0 +1,37 @@
+# ButterSus .dotfiles
+
+This repository contains configuration files and scripts for my amazing unixporn linux setup
+
+## Important Notes
+
+Make sure to run `sudo pacman -Syu` just in case. My script doesn't do that for security reasons, you know
+
+## Installation
+
+There is a pure minimum needed for any CLI environment
+
+1. Clone this repository:
+   ```bash
+   git clone https://github.com/ButterSus/.dotfiles.git
+   cd .dotfiles
+   ```
+
+2. Install essential packages:
+   ```bash
+   bash scripts/arch-cli-setup.sh
+   ```
+
+3. Set up development environment:
+   ```bash
+   # Install NeoVim and development tools
+   bash scripts/nvim-setup.sh
+   ```
+
+## Directory Structure
+- `scripts/` - Installation and management scripts
+  - `arch-cli-setup.sh` - Basic Arch Linux CLI setup
+  - `nvim-setup.sh` - NeoVim and development tools setup
+- `packages/` - Configurations managed by GNU Stow
+  - `dev/` - Development environment configs (git, nvim, etc.)
+
+I'll extend this list and file later
---
+++ scripts/arch-cli-setup.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+################################################################
+# 📜 Arch Linux Package Install Script                         #
+################################################################
+# Installs listed packages on Arch-based systems via Pacman    #
+# Also updates the cache database and existing applications    #
+# For more info, see: https://wiki.archlinux.org/title/Pacman  #
+################################################################
+
+# Function to handle Ctrl+C
+cleanup() {
+    echo -e "\nInstallation aborted by user"
+    exit 1
+}
+
+# Set up trap for Ctrl+C
+trap cleanup SIGINT SIGTERM
+
+# Check if running as root, and prompt for password if not
+if [ "$EUID" -ne 0 ]; then
+    echo "Elevated permissions are required to adjust system settings."
+    echo "Please enter your password..."
+    sudo -v
+    if [ $? -eq 1 ]; then
+        echo "Exiting, as not being run as sudo"
+        exit 1
+    fi
+fi
+
+# Check pacman is installed
+if ! hash pacman 2> /dev/null; then
+    echo "Pacman doesn't seem to be present on your system. Exiting..."
+    exit 1
+fi
+
+# Update package database
+echo "Updating package database..."
+sudo pacman -Syy
+
+# Basic CLI utilities that are essential for comfortable system usage
+ESSENTIAL_PACKAGES=(
+    # Core utilities
+    'man-db' 'man-pages' 'texinfo' 'vi' 'vim' 'nano' 'git' 'sudo' 'tree' 'less'
+    'curl' 'openssh' 'rsync' 'grub' 'arch-install-scripts' 'fastfetch' 'zsh'
+    'fzf' 'zip' 'unzip' 'wget' 'which' 'sed' 'gzip' 'archlinux-keyring' 'stow'
+    
+    # Additional useful tools
+    'tmux' 'ripgrep' 'btop'
+)
+
+echo "Installing essential CLI utilities..."
+sudo pacman -S --needed "${ESSENTIAL_PACKAGES[@]}"
+
+# Ask about base-devel
+read -p "Do you need base-devel? [Y/n] " base_devel
+if [[ $base_devel =~ ^[Nn]$ ]]; then
+    echo "Skipping base-devel installation"
+else
+    echo "Installing base-devel..."
+    sudo pacman -S --needed base-devel
+fi
+
+# Ask about cmake
+read -p "Do you need cmake? [y/N] " cmake
+if [[ $cmake =~ ^[Yy]$ ]]; then
+    echo "Installing cmake..."
+    sudo pacman -S --needed cmake
+fi
+
+# Clear unused package caches
+read -p "Would you like to clear unused package caches? [y/N] " clear_cache
+if [[ $clear_cache =~ ^[Yy]$ ]]; then
+    echo "Clearing package caches..."
+    sudo pacman -Sc --noconfirm
+    if command -v paccache &> /dev/null; then
+        paccache -r
+    fi
+fi
+
+echo "Finished installing packages."
